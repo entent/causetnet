75fd387706c5efd999f91320cda1a9cae5540f71
Anton van Niekerk <avanniekerk@perimeterinstitute.ca>
2018-04-10 23:53:23 -0600

    updated the README file and added plots of the causal diamond and latent encoding of the test set

--- causdiamplot.py
+++ causdiamplot.py
@@ -9,14 +9,15 @@ import causet as cs
 d = 2
 c, t, x = cs.causDiam(1000, d)
 r = np.linalg.norm(x, axis = 0)
+x = x.reshape((1000,))
 t= t.reshape((1000,))
 
-plt.plot(r, t, 'bo')
+plt.plot(x, t, 'bo')
 
 
-plt.xlabel('r')
+plt.xlabel('x')
 plt.ylabel('t')
 plt.title('The causal diamond causal set')
-#plt.grid(True)
-#plt.savefig("test.png")
+plt.axes().set_aspect('equal', 'datalim')
+plt.savefig("causaldiamond2d.png")
 plt.show()
